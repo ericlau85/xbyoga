d3780e808959758249e1977bfbf8bb9ae7079f9b
ericlau85 <ericlau85@icloud.com>
2025-12-05 09:28:59 +0800

    添加Bing网站验证码

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+          <meta name="msvalidate.01" content="A7D7A61D8BE6370E31BF3581BC0D5499" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body style={{
